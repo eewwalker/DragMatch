11ea5479d292a47a2f2c1486887b0f8d12f5cec9
Emily Walker <ewalker415@gmail.com>
2024-01-22 17:08:50 -0800

    inital commit and some functionality

---
+++ memory-game.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/** Memory game: find matching pairs of cards and flip both of them. */
+
+const FOUND_MATCH_WAIT_MSECS = 1000;
+const COLORS = [
+  "red", "blue", "green", "orange", "purple",
+  "red", "blue", "green", "orange", "purple",
+];
+//call to shuffle colors array
+const colors = shuffle(COLORS);
+
+createCards(colors);
+
+
+/** Shuffle array items in-place and return shuffled array. */
+
+function shuffle(items) {
+  // This algorithm does a "perfect shuffle", where there won't be any
+  // statistical bias in the shuffle (many naive attempts to shuffle end up not
+  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
+  // you're interested, you can learn about it, but it's not important.
+
+  for (let i = items.length - 1; i > 0; i--) {
+    // generate a random index between 0 and i
+    let j = Math.floor(Math.random() * i); //0-9
+    // swap item at i <-> item at j
+    [items[i], items[j]] = [items[j], items[i]];
+  }
+
+  return items;
+}
+
+/** Create card for every color in colors (each will appear twice)
+ *
+ * Each div DOM element will have:
+ * - a class with the value of the color
+ * - a click event listener for each card to handleCardClick
+ */
+
+function createCards(colors) {
+  const gameBoard = document.getElementById("game");
+  //iterate through shuffledColors arr and create div for each card(ele)
+  for (let color of colors) {
+    const div = document.createElement('div');
+    div.classList.add(color);
+    gameBoard.append(div);
+    div.addEventListener('click', flipCard);
+  }
+}
+
+/** Flip a card face-up. */
+
+function flipCard(card) {
+  const cardDiv = card.target;
+  const color = cardDiv.getAttribute('class');
+  cardDiv.style.backgroundColor = color;
+}
+
+/** Flip a card face-down. */
+
+function unFlipCard(card) {
+  // ... you need to write this ...
+}
+
+/** Handle clicking on a card: this could be first-card or second-card. */
+
+function handleCardClick(evt) {
+  // ... you need to write this ...
+}
